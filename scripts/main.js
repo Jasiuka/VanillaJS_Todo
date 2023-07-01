c40950c58ad36c9954b1e0bf2171820f4027a642
Jasiuka <luksjasiukaitis@gmail.com>
2023-07-01 14:22:38 +0300

    some styling fixes

--- scripts/main.js
+++ scripts/main.js
@@ -120,13 +120,7 @@ pagesBox.addEventListener("click", (e) => {
 });
 // Key events
 addEventListener("keydown", (e) => {
-  const key = e.key;
-  console.log(key);
-  if (
-    (key === "Enter" && input.value.trim() !== "") ||
-    (key.keyCode === 13 && input.value.trim() !== "")
-  ) {
-    e.preventDefault();
+  if (key === "Enter" && input.value.trim() !== "") {
     document.querySelector(".pages__add-todo-button").click();
     animationFunction.moveInputLabel(inputLabel);
   }
